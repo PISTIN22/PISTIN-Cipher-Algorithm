41c5f764bcc1260734bb0bc9760dc0c1afdcfcd7
Atakan Adalı <atakan_adali@hotmail.com.tr>
2024-06-02 01:07:45 +0300

    Add files via upload

---
+++ KendiAlgoritmam/KendiAlgoritmam/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace KendiAlgoritmam
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 115);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1107, 210);
+            this.textBox1.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(110, 33);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(335, 31);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(110, 84);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(335, 31);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(110, 134);
+            this.numericUpDown3.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(335, 31);
+            this.numericUpDown3.TabIndex = 3;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 84);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Girilecek kelime veya dosya";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "a değeri";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 86);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "b değeri";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 136);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "c değeri";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 58);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(178, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 58);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(483, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 58);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(477, 370);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(644, 264);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(690, 339);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(250, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Şifreli kelime veya dosya";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(594, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 58);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(705, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(105, 58);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.numericUpDown3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 334);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(453, 303);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Anahtar Değerler";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1135, 649);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ПИСТИН Cipher Algorithm & Program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ KendiAlgoritmam/KendiAlgoritmam/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KendiAlgoritmam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        char[] alfabe = { 'a', 'b', 'c', 'ç', 'd', 'e', 'f', 'g', 'ğ',
+            'h', 'ı', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'ö', 'p', 'q', 'r',
+            's', 'ş', 't', 'u', 'ü', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'Ğ', 'H', 'I',
+            'İ', 'J', 'K', 'L', 'M', 'N', 'O', 'Ö', 'P', 'Q', 'R',
+            'S', 'Ş', 'T', 'U', 'Ü', 'V', 'W', 'X', 'Y', 'Z' };
+        public int a;
+        public int b;
+        public int c;
+        private string dosyaYolu = string.Empty;
+
+
+        private string Sifrele(string metin, int anahtar1, int anahtar2, int anahtar3)
+        {
+            try
+            {
+
+                string sifrelenmisMetin = "";
+                for (int i = 0; i < metin.Length; i++)
+                {
+                    char karakter = metin[i];
+
+                    int index = Array.IndexOf(alfabe, karakter);
+                    if (index != -1)
+                    {
+                        int sifrelenmisIndex = (index + anahtar1 - anahtar2 + anahtar3) % alfabe.Length;
+                        anahtar3 += anahtar3;
+                        if (sifrelenmisIndex < 0) sifrelenmisIndex += alfabe.Length;
+
+                        char sifrelenmisKarakter = alfabe[sifrelenmisIndex];
+                        sifrelenmisMetin += alfabe[sifrelenmisIndex];
+                    }
+                    else
+                    {
+                        sifrelenmisMetin += karakter;
+                    }
+                }
+                return sifrelenmisMetin;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Şifrelenirken bir hata oluştu" +
+                    " Hata: " + ex.Message);
+                return "";
+            }
+        }
+
+        private string Desifrele(string sifrelenmisMetin, int anahtar1, int anahtar2, int anahtar3)
+        {
+            try
+            {
+                string cozulmusMetin = "";
+                for (int i = 0; i < sifrelenmisMetin.Length; i++)
+                {
+                    char karakter = sifrelenmisMetin[i];
+                    int index = Array.IndexOf(alfabe, karakter);
+                    if (index != -1)
+                    {
+                        int cozulmusIndex = (index - anahtar1 + anahtar2 - anahtar3) % alfabe.Length;
+                        anahtar3 += anahtar3;
+                        if (cozulmusIndex < 0) cozulmusIndex += alfabe.Length;
+
+                        char cozulmusKarakter = alfabe[cozulmusIndex];
+                        while (cozulmusIndex < 0)
+                        {
+                            cozulmusIndex += alfabe.Length;
+                        }
+                        cozulmusMetin += alfabe[cozulmusIndex];
+                    }
+                    else
+                    {
+                        cozulmusMetin += karakter;
+                    }
+                }
+                return cozulmusMetin;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Deşifrelerken bir hata oluştu" +
+                    " Hata: " + ex.Message);
+                return "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string girilenKelime = textBox1.Text;
+            int a = (int)numericUpDown1.Value;
+            int b = (int)numericUpDown2.Value;
+            int c = (int)numericUpDown3.Value;
+            string sifreliKelime = Sifrele(girilenKelime, a, b, c);
+            textBox2.Text = sifreliKelime;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string girilenKelime = textBox1.Text;
+            int a = (int)numericUpDown1.Value;
+            int b = (int)numericUpDown2.Value;
+            int c = (int)numericUpDown3.Value;
+            string desifreliKelime = Desifrele(girilenKelime, a, b, c);
+            textBox2.Text = desifreliKelime;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "Şifrele";
+            button2.Text = "Şifre Çöz";
+            button3.Text = "Dosya ekle";
+            button4.Text = "Dosya Kaydet";
+            button5.Text = "Temizle";
+            numericUpDown1.ValueChanged += NumericUpDown1_ValueChanged;
+            numericUpDown2.ValueChanged += NumericUpDown2_ValueChanged;
+            numericUpDown3.ValueChanged += NumericUpDown3_ValueChanged;
+
+        }
+
+        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            a = (int)numericUpDown1.Value;
+            UpdateFileContent();
+        }
+
+        private void NumericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            b = (int)numericUpDown2.Value;
+            UpdateFileContent();
+        }
+
+        private void NumericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            c = (int)numericUpDown3.Value;
+            UpdateFileContent();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string dosyaYolu = openFileDialog.FileName;
+                try
+                {
+                    string metin = File.ReadAllText(dosyaYolu);
+                    textBox1.Text = metin;
+
+
+                    ExtractKeysFromText(metin);
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show("Dosya okunurken bir hata oluştu: " + ex.Message);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string dosyaYolu = saveFileDialog.FileName;
+                try
+                {
+                    File.WriteAllText(dosyaYolu, textBox2.Text);
+                    MessageBox.Show("Dosya başarıyla kaydedildi.");
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show("Dosya kaydedilirken bir hata oluştu: " + ex.Message);
+                }
+            }
+        }
+
+        private void ExtractKeysFromText(string metin)
+        {
+            bool numericUpDownEnabled = true;
+
+            string[] lines = metin.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string line in lines)
+            {
+                if (line.Contains("⁕"))
+                {
+                    a = ExtractValue(line, '⁕');
+                    numericUpDownEnabled = false;
+                }
+                else if (line.Contains("※"))
+                {
+                    b = ExtractValue(line, '※');
+                    numericUpDownEnabled = false;
+                }
+                else if (line.Contains("⁜"))
+                {
+                    c = ExtractValue(line, '⁜');
+                    numericUpDown3.Enabled = false;
+                }
+                if (numericUpDownEnabled)
+                {
+                    numericUpDown1.Enabled = true;
+                    numericUpDown2.Enabled = true;
+                    numericUpDown3.Enabled = true;
+                }
+                else
+                {
+                    numericUpDown1.Enabled = false;
+                    numericUpDown2.Enabled = false;
+                    numericUpDown3.Enabled = false;
+                }
+            }
+
+
+            numericUpDown1.Value = a;
+            numericUpDown2.Value = b;
+            numericUpDown3.Value = c;
+        }
+
+        private int ExtractValue(string line, char keyChar)
+        {
+            int value = 0;
+            int index = line.IndexOf(keyChar);
+            if (index != -1 && index + 1 < line.Length)
+            {
+                string valueStr = line.Substring(index + 1).Trim();
+                int.TryParse(valueStr, out value);
+            }
+            return value;
+        }
+
+        private void UpdateFileContent()
+        {
+            if (!string.IsNullOrEmpty(dosyaYolu))
+            {
+                try
+                {
+                    string[] lines = File.ReadAllLines(dosyaYolu);
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        if (lines[i].Contains("⁕"))
+                        {
+                            lines[i] = UpdateLineValue(lines[i], '⁕', a);
+                        }
+                        else if (lines[i].Contains("※"))
+                        {
+                            lines[i] = UpdateLineValue(lines[i], '※', b);
+                        }
+                        else if (lines[i].Contains("⁜"))
+                        {
+                            lines[i] = UpdateLineValue(lines[i], '⁜', c);
+                        }
+
+                    }
+                    File.WriteAllLines(dosyaYolu, lines);
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show("Dosya güncellenirken bir hata oluştu: " + ex.Message);
+                }
+            }
+        }
+
+        private string UpdateLineValue(string line, char keyChar, int newValue)
+        {
+            int index = line.IndexOf(keyChar);
+            if (index != -1)
+            {
+                return line.Substring(0, index + 1) + newValue.ToString();
+            }
+            return line;
+        }
+
+
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
